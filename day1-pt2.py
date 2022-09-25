990d08c4e818409069a75b23405e126c6a6c226f
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2022-09-25 08:31:25 +0200

    refactor day1-pt2

--- day1-pt2.py
+++ day1-pt2.py
@@ -1,23 +1,28 @@
 from more_itertools import windowed
 from typing import Dict, List, Tuple
 
-
-file = open('day1-input.txt', 'r')
-depthsStrs = file.readlines()
-depths = [int(depthStr) for depthStr in depthsStrs]
-
 def window_depth_sums(depths: List[int]):
     windows = windowed(depths, n=3)
     return [sum(window) for window in windows]
 
-windowDepths = window_depth_sums(depths)
+def num_increases(ints: List[int]):
+    result = 0
+    prev = None
+    for num in ints:
+        if prev is not None and num > prev:
+            result += 1
+        prev = num
+    return result
 
-result = 0
-prev = None
-for depth in windowDepths:
-    if prev is not None and depth > prev:
-        result += 1
-    prev = depth
+def depths_from_file():
+    file = open('day1-input.txt', 'r')
+    depthStrs = file.readlines()
+    return [int(depthStr) for depthStr in depthStrs]
+
+
+depths = depths_from_file()
+windowDepths = window_depth_sums(depths)
+result = num_increases(windowDepths)
 
 print(result)
 print("should be 1571")
