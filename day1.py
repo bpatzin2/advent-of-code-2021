8c8081953ff8d4d64c31eccf06e26c3bd515d2ac
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2022-10-01 11:22:07 +0200

    add types to day1pt1

--- day1.py
+++ day1.py
@@ -1,9 +1,13 @@
-file = open('day1-input.txt', 'r')
-depthsStrs = file.readlines()
-depths = map(int, depthsStrs)
+from typing import List
+
+def depths_from_file() -> List[int]:
+    file = open('day1-input.txt', 'r')
+    depthStrs = file.readlines()
+    return [int(depthStr) for depthStr in depthStrs]
 
 result = 0
 prev = None
+depths = depths_from_file()
 for depth in depths:
     if prev is not None and depth > prev:
         result += 1
